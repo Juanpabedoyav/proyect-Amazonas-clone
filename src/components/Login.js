735373dbe7199a8930ed1ccd8f54f6a825b7a6a6
Juan Pa Bedoya V <bedoyavascojuanpablo@gmail.com>
2021-11-28 22:36:28 -0500

    sse actualiza NavBar

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
-import FacebookLogin from 'react-facebook-login';
 import {
     FormControl,
     FormLabel,
-    FormErrorMessage,
+    // FormErrorMessage,
     Input,
-    FormHelperText,
+    // FormHelperText,
   } from "@chakra-ui/react"
   import { Button } from "@chakra-ui/react"
 import { StyleForm } from '../styles/Form.Style';
@@ -13,13 +12,7 @@ import { Link } from 'react-router-dom';
 const Login = () => {
     const [perfil, setPerfil] = useState({})
 
-    const responseFacebook = (response) => {
-        console.log(response);
-        const res = response.picture;
-        const{data} = res;
-        console.log(data );
-        setPerfil(data);
-      }
+   
       
     return (
         <StyleForm>
@@ -39,15 +32,7 @@ const Login = () => {
   />
 </FormControl>
     <Button className="create-button" colorScheme="blue">Iniciar Sesión</Button>
-<FacebookLogin
-    appId="3341008039459589"
-    autoLoad={false}
-    fields="name,email,picture"
-    // onClick={componentClicked}
-    callback={responseFacebook} 
-        icon="fa-facebook"
-        textButton='Ingresar con Facebook'
-    />
+
     <p>Si no tienes cuenta 
     <Link to="/registro">Registrarse</Link>
     </p>
